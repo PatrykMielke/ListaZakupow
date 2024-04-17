b67552427e69fce84109cf18da29ca3142f2dbe5
Patryk Mielke <UL0272601@edu.uni.lodz.pl>
2024-04-18 00:02:45 +0200

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,37 +30,126 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.wyszukiwarka = new System.Windows.Forms.ComboBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonNowyProdukt = new System.Windows.Forms.Button();
+            this.buttonUsunZlisty = new System.Windows.Forms.Button();
+            this.ilosc = new System.Windows.Forms.NumericUpDown();
+            this.Nazwa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iloscKolumna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ilosc)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 77);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(146, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(121, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Produkt";
+            this.label1.Text = "Lista produktów";
             // 
             // wyszukiwarka
             // 
+            this.wyszukiwarka.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.wyszukiwarka.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.wyszukiwarka.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.wyszukiwarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.wyszukiwarka.FormattingEnabled = true;
-            this.wyszukiwarka.Location = new System.Drawing.Point(101, 74);
+            this.wyszukiwarka.Location = new System.Drawing.Point(60, 162);
             this.wyszukiwarka.Name = "wyszukiwarka";
-            this.wyszukiwarka.Size = new System.Drawing.Size(295, 21);
+            this.wyszukiwarka.Size = new System.Drawing.Size(295, 290);
             this.wyszukiwarka.TabIndex = 1;
+            this.wyszukiwarka.KeyDown += new System.Windows.Forms.KeyEventHandler(this.wyszukiwarka_KeyDown);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nazwa,
+            this.iloscKolumna});
+            this.dataGrid.Location = new System.Drawing.Point(719, 107);
+            this.dataGrid.MultiSelect = false;
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.Size = new System.Drawing.Size(541, 463);
+            this.dataGrid.TabIndex = 2;
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(1014, 607);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(161, 82);
+            this.buttonDodaj.TabIndex = 3;
+            this.buttonDodaj.Text = "Dodaj zamówienie";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // buttonNowyProdukt
+            // 
+            this.buttonNowyProdukt.Location = new System.Drawing.Point(375, 279);
+            this.buttonNowyProdukt.Name = "buttonNowyProdukt";
+            this.buttonNowyProdukt.Size = new System.Drawing.Size(61, 35);
+            this.buttonNowyProdukt.TabIndex = 4;
+            this.buttonNowyProdukt.Text = "Dodaj nowy";
+            this.buttonNowyProdukt.UseVisualStyleBackColor = true;
+            this.buttonNowyProdukt.Click += new System.EventHandler(this.buttonNowyProdukt_Click);
+            // 
+            // buttonUsunZlisty
+            // 
+            this.buttonUsunZlisty.Location = new System.Drawing.Point(770, 607);
+            this.buttonUsunZlisty.Name = "buttonUsunZlisty";
+            this.buttonUsunZlisty.Size = new System.Drawing.Size(161, 82);
+            this.buttonUsunZlisty.TabIndex = 5;
+            this.buttonUsunZlisty.Text = "Usuń produkt z listy";
+            this.buttonUsunZlisty.UseVisualStyleBackColor = true;
+            this.buttonUsunZlisty.Click += new System.EventHandler(this.buttonUsunZlisty_Click);
+            // 
+            // ilosc
+            // 
+            this.ilosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ilosc.Location = new System.Drawing.Point(361, 163);
+            this.ilosc.Name = "ilosc";
+            this.ilosc.Size = new System.Drawing.Size(120, 26);
+            this.ilosc.TabIndex = 6;
+            // 
+            // Nazwa
+            // 
+            this.Nazwa.HeaderText = "Nazwa produktu";
+            this.Nazwa.Name = "Nazwa";
+            this.Nazwa.Width = 400;
+            // 
+            // iloscKolumna
+            // 
+            this.iloscKolumna.HeaderText = "Ilość";
+            this.iloscKolumna.Name = "iloscKolumna";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(1424, 861);
+            this.Controls.Add(this.ilosc);
+            this.Controls.Add(this.buttonUsunZlisty);
+            this.Controls.Add(this.buttonNowyProdukt);
+            this.Controls.Add(this.buttonDodaj);
+            this.Controls.Add(this.dataGrid);
             this.Controls.Add(this.wyszukiwarka);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Zamowienia";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ilosc)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +159,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox wyszukiwarka;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonNowyProdukt;
+        private System.Windows.Forms.Button buttonUsunZlisty;
+        private System.Windows.Forms.NumericUpDown ilosc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nazwa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iloscKolumna;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Zbieracz_zamowien
 {
     public partial class Form1 : Form
     {
+        private static readonly string CONN = "Data Source=.;Initial Catalog=ListaZakupow;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +21,79 @@ namespace Zbieracz_zamowien
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            wyszukiwarka.AutoCompleteCustomSource = new AutoCompleteStringCollection();
-            var st = from s in db;
+            List<string> product_list = new List<string>();
+
+            SqlConnection conn = new SqlConnection(CONN);
+            SqlCommand cmd = new SqlCommand("select CONCAT([Nazwa produktu], ' ', Jednostka) from Produkty", conn);
+            conn.Open();
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    product_list.Add(reader.GetString(0));
+                }
+            }
+
+
+            //wyszukiwarka.AutoCompleteCustomSource = new AutoCompleteStringCollection();
+            
+            
+            wyszukiwarka.DataSource = product_list;
+        }
+
+        private void wyszukiwarka_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // if contains -> pokazać w datagridzie z użyciem timera ig
+
+                if (wyszukiwarka.SelectedItem==null || !wyszukiwarka.Items.Contains(wyszukiwarka.SelectedItem.ToString())) return;
+
+                string selectedItem = wyszukiwarka.SelectedItem.ToString();
+
+                if(dataGrid.Rows.Count > 0)
+                {
+                    foreach (DataGridViewRow row in dataGrid.Rows)
+                    {
+                        if (row.Cells[0].Value.ToString() == selectedItem ) return;
+                    }
+                }
+                
+                int rowIndex = dataGrid.Rows.Add(selectedItem, ilosc.Value);
+                dataGrid.Rows[rowIndex].Tag = rowIndex;
+
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(CONN);
+
+            // get new ID -> int new_order_id = select max(id) from zamowienia
+
+            //SqlCommand()
+        }
+
+        private void buttonUsunZlisty_Click(object sender, EventArgs e)
+        {
+            if (dataGrid.Rows.Count < 1) return;
+
+            int selectedRow = (int)dataGrid.SelectedRows[0].Tag;
+            dataGrid.Rows.RemoveAt(selectedRow);
+        }
+
+        private void buttonNowyProdukt_Click(object sender, EventArgs e)
+        {
+            if (wyszukiwarka.SelectedItem == null || wyszukiwarka.SelectedItem.ToString().Length == 0) return;
+
+            string newProduct = wyszukiwarka.SelectedItem.ToString();
+
+            // inserts kg/szt
         }
     }
 }
